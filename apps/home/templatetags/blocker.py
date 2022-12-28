a9e6bde6215bdfee406d4c9f26196a4997d6d6b4
anassfresco <anasbahi2015@gmail.com>
2022-12-28 16:14:50 +0100

    lasat

--- apps/home/templatetags/blocker.py
+++ apps/home/templatetags/blocker.py
@@ -1,70 +1,49 @@
-# from urllib import request
-# from django import template
-# from django.shortcuts import redirect
-# from django.contrib.auth.models import User
-# from sys import platform
-# import requests
-# import os
-# from ..models import DurationUsage
-# import subprocess
-# liste_blocked=[x.ip_address for x in DurationUsage.objects.filter(author=request.user)]
-# if platform=="win32":
+import time
+import sys
+import platform
 
-#     register=template.Library()
-
-#     @register.filter
-#     def blocker(string) :
-#         import time
-#         from datetime import datetime as dt
-#         #Windows host file path
-#         hostsPath=r"C:/Windows/System32/drivers/etc/hosts"
-#         redirect="127.0.0.1"
-#         #Add the website you want to block, in this list
-#         websites=string
-#     #Duration during which, website blocker will work is from 9 am to 6 pm
-#         if dt(dt.now().year,dt.now().month,dt.now().day,9) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,18):
-#             print ("...")
-#             with open(hostsPath,'r+') as file:
-#                 content = file.read()
-#                 if websites in content:
-#                     pass
-#                 else:
-#                     file.write(redirect+" "+websites+"\n")
-#         else:
-#             with open(hostsPath,'r+') as file:
-#                 content = file.readlines()
-#                 file.seek(0)
-#                 for line in content:
-#                     if not websites in line :
-#                         file.write(line)
-#                 file.truncate()
-#             print ("!!!")
-# elif platform == "linux" or platform == "linux2":
-#     if len(liste_blocked)!=0:
-#         for i in liste_blocked:
-#             build_str='echo 127.0.01'+' '+str(i)+'>> /etc/hosts'
-#             os.system(build_str)
-# elif platform == "darwin":
-
-#     def block_website(website):
-#         with open("/etc/hosts", "a") as f:
-#             for i in :
-#                 f.write(f"127.0.0.1 {i}\n")
-#         subprocess.run(["dscacheutil", "-flushcache"])
-
-# def unblock_website(website):
-#     with open("/etc/hosts", "r") as f:
-#         lines = f.readlines()
-#     with open("/etc/hosts", "w") as f:
-#         for line in lines:
-#             if website not in line:
-#                 f.write(line)
-#     subprocess.run(["dscacheutil", "-flushcache"])
+#function to fetch the URLs that have been saved in urllist.txt for blocking
 
 
-        
+#fetching the URLs, defining the redirection to localhost 127.0.0.1
 
+redirect = "127.0.0.1"
 
+#identifying the underlying Operating System to decide the directory the hosts file resides in
+#Currently working for Microsoft Windows, Linux Distributions and MacOS
+if platform.system() == 'Windows':
+    host_file = r"C:\Windows\System32\drivers\etc\hosts"
+elif platform.system() == "Linux" or platform.system() == "darwin":
+    host_file = "/etc/hosts"
+else:
+    print("Expected your OS to be Windows, Linux or MacOS but found something else, hence quitting...")
+    sys.exit(1)
 
+#function that will block or remove blocking based on user request
+def block(string):
+	while True:
+		with open(host_file, 'r+') as hostentry:
+			hosts = hostentry.read() #reading the host entries to confirm if the required URLs are present or not
+			#adding the urls from DB in hosts file and adding redirection to localhost to make them inaccessible
+			for url in [string]:
+				if url not in hosts:
+					hostentry.write(redirect+' '+url+'\n')
+		try:
+			time.sleep(50)
+		except KeyboardInterrupt:
+			#catching the ctrl + c (quit) from user, asking for confirmation
+			quitting = input("Do you really want to disable content blocking?[Y/y or N/n]")
+			if (quitting == "Y" or quitting == "y"):
+				#clearing the entries made during blocking phase
+				with open(host_file, 'r+') as hostentry:
+					hosts = hostentry.readlines()
+					hostentry.seek(0)
+					for host in hosts:
+						if not any(url in host for url in [string]):
+							hostentry.write(host)
+					hostentry.truncate()
+				print("Cool!!\nhave a great time!")
+				sys.exit(1)
+			else:
+				continue
 
-    
--- apps/home/templatetags/log_generator.py
+++ apps/home/templatetags/log_generator.py
@@ -5,45 +5,50 @@ import pyshark
 from datetime import  datetime
 
 
-filed_name = ["time", "ip_source", "ip_dest", "protocol"]
+
+
+def generate_logs():
+
+
+    filed_name = ["time", "ip_source", "ip_dest", "protocol"]
 
 
 
-with open('data.csv', 'w') as csv_file:
-    csv_writer = csv.DictWriter(csv_file, fieldnames=filed_name)
-    csv_writer.writeheader()
+    with open('data.csv', 'w') as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=filed_name)
+        csv_writer.writeheader()
 
 
 
-networkInterface = "Wi-Fi"
+    networkInterface = "Wi-Fi"
 
 # define capture object
-capture = pyshark.LiveCapture(interface=networkInterface)
+    capture = pyshark.LiveCapture(interface=networkInterface)
 
-print("listening on %s" % networkInterface)
+    print("listening on %s" % networkInterface)
 
-for packet in capture.sniff_continuously():
+    for packet in capture.sniff_continuously():
     # adjusted output
-    localtime = time.asctime(time.localtime(time.time()))
+        localtime = time.asctime(time.localtime(time.time()))
 
     # get packet content
-    protocol = packet.transport_layer  # protocol type
-    src_addr = packet.ip.src  # source address
-    src_port = packet[protocol].srcport  # source port
-    dst_addr = packet.ip.dst  # destination address
-    dst_port = packet[protocol].dstport
-    with open('data.csv', 'a') as csv_file:
-        csv_writer = csv.DictWriter(csv_file, fieldnames=filed_name)
+        protocol = packet.transport_layer  # protocol type
+        src_addr = packet.ip.src  # source address
+        src_port = packet[protocol].srcport  # source port
+        dst_addr = packet.ip.dst  # destination address
+        dst_port = packet[protocol].dstport
+        with open('data.csv', 'a') as csv_file:
+            csv_writer = csv.DictWriter(csv_file, fieldnames=filed_name)
 
-        info = {
+            info = {
             "time":localtime,
-            "ip_source":src_addr,
-            "ip_dest":dst_addr,
+            "ip_source":src_addr+":"+src_port,
+            "ip_dest":dst_addr+""+dst_port,
             "protocol":protocol
 
-        }
-        print(info)
+            } 
+            print(info)
 
-        csv_writer.writerow(info)
-        time.sleep(1)
+            csv_writer.writerow(info)
+            time.sleep(1)
 
